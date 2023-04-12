036dee6441230b61b6bf5421d953662bdba1f3cf
Jakub Bembenek <jakub.bembenekk@gmail.com>
2023-04-12 18:34:41 +0200

    Added Game class tests

---
+++ tests/game_test.py
@@ -0,0 +1,100 @@
+import pytest
+
+from hive.engine.game import Game, InvalidMove
+from hive.engine.notation import GameState, GameString, PieceColor
+
+
+@pytest.fixture
+def game() -> Game:
+    return Game()
+
+
+@pytest.mark.parametrize(
+    "gamestring",
+    [
+        pytest.param("Base;NotStarted;White[1]", id="not_started"),
+        pytest.param(
+            "Base;InProgress;White[3];wS1;bG1 -wS1;wA1 wS1/;bG2 /bG1", id="in_progress"
+        ),
+        pytest.param(
+            "Base;Draw;White[7];wQ;bQ -wQ;wS1 wQ-;bS1 -bQ;wS2 wQ/;bS2 \\bQ;wG1 wQ\\;bG1 /bQ;wA1 wS1-;bA1 -bS1;wA1 bQ/;bA1 /wQ",
+            id="draw",
+        ),
+        pytest.param(
+            "Base;BlackWins;White[6];wQ;bG1 wQ-;wG1 -wQ;bQ bG1\\;wG2 /wQ;bA1 bG1-;wA1 \\wQ;bQ wQ\\;wG2 wQ/;bA1 /wQ",
+            id="black_wins",
+        ),
+        pytest.param(
+            "Base;WhiteWins;Black[5];wG1;bQ wG1-;wQ /wG1;bG1 bQ-;wA1 -wG1;bG2 bQ\\;wQ /bQ;bA1 bQ/;wA1 \\bQ",
+            id="white_wins",
+        ),
+    ],
+)
+def test_after_load_game_state_equals_gamestring(game: Game, gamestring: str):
+    game.load_game(gamestring)
+    orig_props = GameString.decompose(gamestring)
+    status_props = GameString.decompose(game.status)
+    assert all(
+        [
+            status_property == orig_property
+            for status_property, orig_property in zip(status_props, orig_props)
+        ]
+    )
+
+
+def test_game_not_started_after_new_game(game: Game):
+    game.new_game()
+
+    properties = GameString.decompose(game.status)
+    gamestate = None
+    for property in properties:
+        if isinstance(property, GameState):
+            gamestate = property
+
+    assert gamestate == GameState.NotStarted
+
+
+@pytest.mark.parametrize(
+    "gamestring",
+    [
+        pytest.param("Base;NotStarted;White[1]", id="not_started"),
+        pytest.param(
+            "Base;InProgress;White[3];wS1;bG1 -wS1;wA1 wS1/;bG2 /bG1", id="in_progress"
+        ),
+        pytest.param(
+            "Base;Draw;White[7];wQ;bQ -wQ;wS1 wQ-;bS1 -bQ;wS2 wQ/;bS2 \\bQ;wG1 wQ\\;bG1 /bQ;wA1 wS1-;bA1 -bS1;wA1 bQ/;bA1 /wQ",
+            id="draw",
+        ),
+        pytest.param(
+            "Base;BlackWins;White[6];wQ;bG1 wQ-;wG1 -wQ;bQ bG1\\;wG2 /wQ;bA1 bG1-;wA1 \\wQ;bQ wQ\\;wG2 wQ/;bA1 /wQ",
+            id="black_wins",
+        ),
+        pytest.param(
+            "Base;WhiteWins;Black[5];wG1;bQ wG1-;wQ /wG1;bG1 bQ-;wA1 -wG1;bG2 bQ\\;wQ /bQ;bA1 bQ/;wA1 \\bQ",
+            id="white_wins",
+        ),
+    ],
+)
+def test_is_status_valid_gamestring(game: Game, gamestring: str):
+    game.load_game(gamestring)
+
+    assert GameString.is_valid(game.status)
+
+
+@pytest.mark.parametrize(
+    "start_moves",
+    [
+        pytest.param([], id="first_move"),
+        pytest.param(["wS1", "bG1 -wS1", "wA1 wS1/"], id="after_three_moves"),
+    ],
+)
+def test_passing_while_having_moves_raises_invalidmove(
+    game: Game, start_moves: list[str]
+):
+    game.new_game()
+
+    for move in start_moves:
+        game.play(move)
+
+    with pytest.raises(InvalidMove):
+        game.pass_move()
