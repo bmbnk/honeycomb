38ec9f829c24b1f28cd3d9f37f2832cdb72f3ffa
Jakub Bembenek <jakub.bembenekk@gmail.com>
2023-04-29 18:05:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-import os
-
 import setuptools
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 with open("honeycomb/_version.py") as f:
-    version = f.read()
+    version_line = f.readline()
+    globals_values = {}
+    exec(version_line, globals_values)
+    version = globals_values["__version__"]
 
 with open("requirements-dev.txt") as f:
     test_requires = [
@@ -22,7 +23,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bmbnk/honeycomb",
-    packages=["honeycomb"],
+    packages=setuptools.find_packages(exclude=["tests"]),
     entry_points={
         "console_scripts": [
             "honeycomb=honeycomb.__main__:main",
